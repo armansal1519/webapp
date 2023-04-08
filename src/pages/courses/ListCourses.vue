0258d6f0ca67b0956d64bbb67dd0f2f9293eec82
arman salehi <armansal1519@gmail.com>
2023-04-08 18:24:17 +0330

    first commit

---
+++ src/pages/courses/ListCourses.vue
@@ -0,0 +1,25 @@
+<template>
+  <h1>hi</h1>
+</template>
+
+<script>
+import {
+  CourseServiceClient,
+  SearchCourseRequest,
+} from "app/proto/course_grpc_web_pb";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "ListCourses",
+  setup() {
+    console.log("hiiiiiiiiiiiiiiii");
+    const client = CourseServiceClient("http://localhost:30032", null, null);
+    client.search(new SearchCourseRequest(), {}, (err, response) => {
+      // handle the response
+      console.log(response);
+    });
+  },
+});
+</script>
+
+<style></style>
